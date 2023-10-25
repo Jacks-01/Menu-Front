b35875b5cea39bbdd760ab044c536fd0c4754148
Jack Stubblefield <jack@roberts1.local>
2023-10-25 11:59:30 -0500

    adjusted prettier formatting: bracket spacing

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './Routes/Routes.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes/Routes.tsx";
 
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-        <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
