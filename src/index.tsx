06ef7e069c852d21dac8b00c2cbb7d26614b2791
Jack Stubblefield <jack011601@gmail.com>
2023-10-25 17:13:13 -0500

    added MUI theme provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Routes.tsx";
+import { ThemeProvider } from "@mui/material";
+import theme from "./theme/theme.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>,
 );
--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -1,19 +1,28 @@
 import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
 
-// A custom theme for this app
-const theme = createTheme({
+/** Think of creating a theme as a two-step composition process:
+    first, you define the basic design options; then,
+    you'll use these design options to compose other options.
+**/
+
+let theme = createTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: '#0052cc',
     },
     secondary: {
-      main: '#19857b',
-    },
-    error: {
-      main: red.A400,
+      main: '#edf2ff',
     },
   },
 });
+
+//* Uncomment this if you want to alter a specific variable in the theme
+// theme = createTheme(theme, {
+//   palette: {
+//     info: {
+//       main: theme.palette.secondary.main,
+//     },
+//   },
+// });
 
 export default theme;
